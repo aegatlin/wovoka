c821087c728e102d5b3dc2daab4911a808c0eb9f
Austin Gatlin <austin@gatlin.io>
2022-04-17 16:08:01 +1000

    First commit.

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Index() {
+  return (
+    <div>
+      <Head>
+        <title>App</title>
+        <meta name="description" content="An app" />
+      </Head>
+      <main className="flex flex-col items-center">
+        <h1 className="text-6xl">App</h1>
+      </main>
+    </div>
+  );
+}
